e66c83e56d0d6f64f52b3c844ecde52c97ad4cc2
Ajinkya-Kunjir <36645323+Ajinkya-Kunjir@users.noreply.github.com>
2018-03-29 15:45:13 -0400

    Add files via upload

---
+++ Angular_classroom/src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import{Hero} from './hero';
+
+export const HEROES:Hero[] =[
+    {id:1,name:'Batman',age:12},
+    {id:2,name:'Superman',age:23},
+    {id:3,name:'Spiderman',age:21},
+    {id:4,name:'Captain America',age:25},
+    {id:5,name:'Black Panther',age:null},
+    {id:6,name:'Ironman',age:22},
+    {id:7,name:'Deadpool',age:22}   
+];
